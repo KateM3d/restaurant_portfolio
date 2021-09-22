86f44a60a7104098e3a72879af443020528aa9c0
KateM3d <ekaterina8medvedeva@gmail.com>
2021-09-22 17:56:11 -0400

    added json for the section about_pros

--- script.js
+++ script.js
@@ -6,14 +6,46 @@ let menuElements = `[
     {"label" : "Contact Us"}
 ]`
 
+
+let aboutPros = `[
+    {
+    "icon" : "img/star.png",
+    "heading" : "Quality",
+    "description" : "Lorem ipsum dolor sit amet, conse cteturad ipi scinelit.Suspen disse vitae ligula quis."
+    },
+    {
+    "icon" : "img/vegetable.png",
+    "heading" : "Food",
+    "description" : "Lorem ipsum dolor sit amet, conse cteturad ipi scinelit.Suspen disse vitae ligula quis."
+    },
+    {
+    "icon" : "img/like.png",
+    "heading" : "Service",
+    "description" : "Lorem ipsum dolor sit amet, conse cteturad ipi scinelit.Suspen disse vitae ligula quis."
+    
+    }
+    
+]`;
+
 document.addEventListener('DOMContentLoaded', function(event) {
-    let menu = JSON.parse(menuElements)
+    let menu = JSON.parse(menuElements);
     let menuContent = '';
 
     for (item of menu) {
         menuContent += `
-            <li class="menu_element"> ${item.label} </li>`
+            <li class="menu_element"> ${item.label} </li>`;
     }
+
+
+    let aboutElements = JSON.parse(aboutPros);
+    let aboutProsInner = document.querySelector('.about_pros');
+    for (aboutElement of aboutElements) {
+        aboutProsInner.innerHTML += `<div class="about_pros_element">
+            <img class="about_icon" src="${aboutElement.icon}" alt="icon"> </img>
+            <h2 class="about_pros_heading"> ${aboutElement.heading} </h2>
+            <p class="about_pros_par"> ${aboutElement.description} </p></div>`
+    }
+
 
     document.querySelector("ul").innerHTML = menuContent;
 
